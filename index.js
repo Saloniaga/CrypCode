8915030b914b6720ef55aa133a685c7765676aa9
Saloniaga <sagarwal9475@gmail.com>
2022-08-05 23:56:01 +0530

    syncronisation of chain

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const request = require("request");
 const BlockChain = require("./blockchain");
 const PubSub = require("./pubsub");
 
 const app = express();
 const blockchain = new BlockChain();
 const pubsub = new PubSub({ blockchain });
+
+const DEFAULT_PORT = 3000;
+
+const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`;
 
 setTimeout(() => pubsub.broadcastChain(), 1000);
 
@@ -22,8 +27,18 @@ app.post("/api/mine", (req, res) => {
   res.redirect("/api/blocks");
 });
 
-const DEFAULT_PORT = 3000;
-
+const syncChains = () => {
+  request(
+    { url: `${ROOT_NODE_ADDRESS}/api/blocks` },
+    (error, response, body) => {
+      if (!error && response.statusCode === 200) {
+        const rootChain = JSON.parse(body);
+        console.log("replace chain on a sync with ", rootChain);
+        blockchain.replaceChain(rootChain);
+      }
+    }
+  );
+};
 let PEER_PORT;
 
 if (process.env.GENERATE_PEER_PORT === "true") {
@@ -33,4 +48,5 @@ const PORT = process.env.PORT || PEER_PORT || DEFAULT_PORT;
 
 app.listen(PORT, () => {
   console.log(`listening at localhost:${PORT}`);
+  syncChains();
 });
