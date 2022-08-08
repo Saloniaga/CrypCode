807c67a3fb1d35bd8087febafea5201a912277ee
Parth Gadoo <p.gadoo@iitg.ac.in>
2022-08-08 19:52:48 +0530

    added_sync_chain_across_all_peers

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const request = require("request");
 const bodyParser = require("body-parser");
 const BlockChain = require("./blockchain");
 const PubSub = require("./pubsub");
@@ -6,6 +7,9 @@ const PubSub = require("./pubsub");
 const app = express();
 const blockchain = new BlockChain();
 const pubsub = new PubSub({ blockchain });
+
+const DEFAULT_PORT = 3000;
+const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`;
 
 setTimeout(() => pubsub.broadcastChain(), 1000);
 
@@ -18,10 +22,32 @@ app.get("/api/blocks", (req, res) => {
 app.post("/api/mine", (req, res) => {
   const { data } = req.body;
   blockchain.addBlock({ data });
+
+  pubsub.broadcastChain();
   res.redirect("/api/blocks");
 });
 
-const PORT = 3000;
+const syncChains = () => {
+  request(
+    { url: `${ROOT_NODE_ADDRESS}/api/blocks` },
+    (error, response, body) => {
+      if (!error && response.statusCode === 200) {
+        const rootChain = JSON.parse(body);
+
+        console.log("replace chain on a sync with", rootChain);
+        blockchain.replaceChain(rootChain);
+      }
+    }
+  );
+};
+let PEER_PORT;
+
+if (process.env.GENERATE_PEER_PORT === "true") {
+  PEER_PORT = DEFAULT_PORT + Math.ceil(Math.random() * 1000);
+}
+
+const PORT = PEER_PORT || DEFAULT_PORT;
 app.listen(PORT, () => {
   console.log(`listening at localhost:${PORT}`);
+  syncChains();
 });
