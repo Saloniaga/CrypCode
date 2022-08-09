0e456b571dd1eff727014d9d51c2311bfd259d2c
Saloniaga <sagarwal9475@gmail.com>
2022-08-09 21:49:45 +0530

    multiple transactions for same user

--- util/crypto-hash.test.js
+++ util/crypto-hash.test.js
@@ -2,13 +2,21 @@ const cryptoHash = require("./crypto-hash");
 
 describe("cryptoHash()", () => {
   it("generates a SHA-256 hashed output", () => {
-    expect(cryptoHash("foo")).toEqual(
-      "b2213295d564916f89a6a42455567c87c3f480fcd7a1c15e220f17d7169a790b"
+    expect(cryptoHash("dummy")).toEqual(
+      "ebbe2dfbb019b8256cb73d309d38f8478b7a3aec5aef544f1aa69087aedb713b"
     );
   });
   it("produces the same hash with the same input arguments in any order", () => {
     expect(cryptoHash("one", "two", "three")).toEqual(
       cryptoHash("three", "one", "two")
     );
+  });
+
+  it("produces a unique hash when the properties have changed on an input", () => {
+    const dummy = {};
+    const originalHash = cryptoHash(dummy);
+    dummy["a"] = "a";
+
+    expect(cryptoHash(dummy)).not.toEqual(originalHash);
   });
 });
--- wallet/index.js
+++ wallet/index.js
@@ -1,16 +1,29 @@
 const { STARTING_BALANCE } = require("../config");
+const Transaction = require("./transaction");
 const { ec, cryptoHash } = require("../util");
 
 class Wallet {
   constructor() {
     this.balance = STARTING_BALANCE;
-
     this.keyPair = ec.genKeyPair();
     this.publicKey = this.keyPair.getPublic().encode("hex");
   }
 
   sign(data) {
     return this.keyPair.sign(cryptoHash(data));
+  }
+
+  createTransaction({ recipient, amount, chain }) {
+    if (chain) {
+      this.balance = Wallet.calculateBalance({
+        chain,
+        address: this.publicKey,
+      });
+    }
+    if (amount > this.balance) {
+      throw new Error("Amount exceeds balance");
+    }
+    return new Transaction({ senderWallet: this, recipient, amount });
   }
 }
 
