6747afd7c1459f5b0499bb1fa16246f99b228334
Parth Gadoo <p.gadoo@iitg.ac.in>
2022-09-01 15:18:43 +0530

    transaction_pool_route_added

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,7 @@ import history from "./history";
 import App from "./components/App";
 import Blocks from "./components/Blocks";
 import ConductTransaction from "./components/ConductTransaction";
-// import TransactionPool from './components/TransactionPool';
+import TransactionPool from "./components/TransactionPool";
 import "./index.css";
 
 render(
@@ -14,7 +14,7 @@ render(
       <Route exact path="/" component={App} />
       <Route path="/blocks" component={Blocks} />
       <Route path="/conduct-transaction" component={ConductTransaction} />
-      {/* <Route path='/transaction-pool' component={TransactionPool} /> */}
+      <Route path="/transaction-pool" component={TransactionPool} />
     </Switch>
   </Router>,
   document.getElementById("root")
